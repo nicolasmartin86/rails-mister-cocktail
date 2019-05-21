b60f213e3c7148c00b4f0fad4383257e8167236f
Nicolas Martin <nicolas.artur.martin@gmail.com>
2019-05-21 17:09:11 +0200

    mister cocktail back end

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'doses/cocktail:references'
-  get 'doses/ingredient:references'
-  get 'doses/description'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails do
-    resources :doses, only: [:new, :create, :destroy]
+    resources :doses, only: [:new, :create]
   end
+  resources :doses, only: [:destroy]
 end
