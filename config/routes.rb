19305a3f3b03df0aaff0507e7ceb3ef94e2006ca
Nicolas Martin <nicolas.artur.martin@gmail.com>
2019-05-22 12:08:35 +0200

    typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'coktails#index'
+  root to: 'cocktails#index'
   resources :cocktails, except: :index do
     resources :doses, only: [:new, :create]
   end
